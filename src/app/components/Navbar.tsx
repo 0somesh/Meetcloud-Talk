e23a082301943acbe82c925b14713d0956f71297
0somesh <0somesh.sh@gmail.com>
2025-08-05 23:58:42 +0530

    modified & added

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,37 @@
+// import Link from "next/link"
+// import React from "react"
+// import Image from "next/image"
+
+// import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+
+// const Navbar = () => {
+//   return (
+//     <nav className="fixed px-6 lg:px-10 py-4 flex-between w-full bg-dark-1 z-50">
+//       <Link href={"/"} className="flex  items-center gap-2">
+//         <Image
+//           src={"window.svg"}
+//           height={32}
+//           width={32}
+//           alt="zoom-clone"
+//           className="max-sm:size-10"
+//         />
+//         <p className="text-[26px] text-white font-extrabold max-sm:hidden">
+//           Zoom Clone
+//         </p>
+//       </Link>
+//       <div className="flex-between gap-4">
+//         <div>
+//           <SignedIn>
+//             <UserButton />
+//           </SignedIn>
+//           <SignedOut>
+//             <UserButton />
+//           </SignedOut>
+//         </div>
+
+//       </div>
+//     </nav>
+//   )
+// }
+
+// export default Navbar
